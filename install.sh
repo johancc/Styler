079e1146ff291cf112a4198c28350f7b783d2ae2
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-03-07 16:47:56 -0500

    Fixed install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 echo "Downloading segmentation data..."
 git submodule init
 git submodule update
-bash CDCL-human-segmentation/fetch_data.sh
-rm CDCL-human-segmentation/input/*
-rm CDCL-human-segmentation/output/*
+bash CDCL-human-part-segmentation/fetch_data.sh
+rm CDCL-human-part-segmentation/input/*
+rm CDCL-human-part-segmentation/output/*
 echo "Installing dependencies..."
 pip3 install -r requirements.txt --user
 
