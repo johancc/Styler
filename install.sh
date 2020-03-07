acb6da8c5e367de6d7e63b40269c97770877bec9
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-03-07 16:45:51 -0500

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Downloading segmentation data..."
+git submodule init
+git submodule update
 bash CDCL-human-segmentation/fetch_data.sh
 rm CDCL-human-segmentation/input/*
 rm CDCL-human-segmentation/output/*
