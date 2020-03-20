be56699baa9f080f5c56ae1ea6c29b9eaf1835c7
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-03-20 12:33:57 -0700

    Implemented original, segmented, and frame merging to create the final output.

--- segmentation.py
+++ segmentation.py
@@ -4,6 +4,15 @@ Runs segmentation on a batch of frames. Wrapper for the CDCL-human-part-segmenta
 import os
 import subprocess
 
+from PIL import Image
+
+# The segmented frame has a dark mask over the whole image, and the
+# human sections have are colored. Thus, we cannot use a simple bitwise_and
+# to create the final segmented image. We thus need to look at how different
+# the pixels are. The threshold is the max rgb difference from the original
+# and segmented image.
+THRESHOLD = 155
+
 
 def segment(frames_folder: str, output_folder):
     for f in [frames_folder, output_folder]:
@@ -20,4 +29,53 @@ def segment(frames_folder: str, output_folder):
 def apply_style_over_segmentation(original_folder: str, style_folder: str,
                                   segmentation_folder: str, output_folder: str,
                                   mode: int):
-    pass
+    """
+    mode - 0 if the human should be styled, 1 if the background should be styled.
+    """
+    original_frames = sorted(os.listdir(original_folder))
+    styled_frames = sorted(os.listdir(style_folder))
+    segmented_frames = sorted(os.listdir(segmentation_folder))
+
+    final_frames = []
+    for i in range(len(original_folder)):
+        original_frame_path = os.path.join(original_folder, original_frames[i])
+        change_frame_path = os.path.join(style_folder, styled_frames[i])
+        segmented_frame_path = os.path.join(segmentation_folder, segmented_frames[i])
+        out_name = os.path.join(output_folder, original_frames[i])
+        merged_frame = merge_difference(original_frame_path, segmented_frame_path, change_frame_path, out_name, mode)
+        final_frames.append(merged_frame)
+
+
+def merge_difference(original_path: str, change_path: str, custom_replacement: str, out_name: str, mode: int = 0):
+    if custom_replacement is None:
+        custom_replacement = change_path
+    original = Image.open(original_path)
+    o_pix = original.load()
+    change = Image.open(change_path)
+    custom = Image.open(custom_replacement)
+    c_pix = custom.load()
+    assert original.size == change.size
+    width, height = original.size
+
+    for x in range(width):
+        for y in range(height):
+            # Change to >= if you want to get the foreground
+            dist = distance_fx(original.getpixel((x, y)), (0, 0, 0))
+            is_foreground = dist < THRESHOLD
+            is_background = dist >= THRESHOLD
+            if mode == 0 and is_foreground:
+                o_pix[x, y] = c_pix[x, y]
+            elif mode == 1 and is_background:
+                o_pix[x, y] = c_pix[x, y]
+
+    original.save(out_name)
+    assert os.path.isfile(out_name)
+    return out_name
+
+
+def distance_fx(a, b):
+    assert len(a) == len(b)
+    dist = 0
+    for i in range(len(a)):
+        dist += abs(a[i] - b[i]) ** 2
+    return dist ** 0.5
