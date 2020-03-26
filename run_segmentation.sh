962f5bb2007be6e3fe11edece318b5c630177b4f
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-03-25 20:40:53 -0700

    Fixed segmentation script

--- run_segmentation.sh
+++ run_segmentation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Args:
 # input folder, output folder
-mv "$1"/* segmentation/input
-cd segmentation/ || exit
+mv "$1"/* CDCL-human-part-segmentation/input
+cd CDCL-human-part-segmentation/ || exit
 python3 inference_7parts.py --scale=1
 cd .. || exit
 mv output/* "$2"
