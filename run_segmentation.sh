470ce8eadcfaa74647d91f7dcd92b708ba7b7e8e
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-03-25 21:03:56 -0700

    Fixed script bonus track

--- run_segmentation.sh
+++ run_segmentation.sh
@@ -4,5 +4,6 @@
 mv "$1"/* CDCL-human-part-segmentation/input
 cd CDCL-human-part-segmentation/ || exit
 python3 inference_7parts.py --scale=1
+rm input/*
 cd .. || exit
-mv output/* "$2"
+mv CDCL-human-part-segmentation/output/* "$2"
