2f810f8bfce47f6a49e343544f06fded3e6427ba
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-03-30 23:27:24 -0700

    Fixed shell script

--- run_segmentation.sh
+++ run_segmentation.sh
@@ -5,7 +5,8 @@ mkdir CDCL-human-part-segmentation/"$3"_input
 cp "$1"/* CDCL-human-part-segmentation/"$3"_input
 cd CDCL-human-part-segmentation/ || exit
 mkdir "$3"_output
-python3 inference_7parts.py --scale=1 --gpus="$4" --input-folder="$3"_input --output-folder="$3"_output
-mv "$3"_output/* ..
-rm "$3"*
+python3 inference_7parts.py --scale=1 --gpus="$4" --input_folder="$3"_input --output_folder="$3"_output
+mv "$3"_output/* ../"$2"
+rmdir "$3"_output
+rmdir "$3"_input
 cd .. || exit
