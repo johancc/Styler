8db1898d8d091271652c5729ee2b946ae6c361e1
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-04-02 17:04:36 -0700

    Runs the more accurate segmentation model now

--- run_segmentation.sh
+++ run_segmentation.sh
@@ -5,7 +5,7 @@ mkdir CDCL-human-part-segmentation/"$3"_input
 cp "$1"/* CDCL-human-part-segmentation/"$3"_input
 cd CDCL-human-part-segmentation/ || exit
 mkdir "$3"_output
-python3 inference_7parts.py --scale=1 --gpus="$4" --input_folder="$3"_input --output_folder="$3"_output
+python3 inference_15parts.py --scale=1 --scale=0.75 --scale=0.5 --scale=0.25 --gpus="$4" --input_folder="$3"_input --output_folder="$3"_output
 mv "$3"_output/* ../"$2"
 rm -r "$3"_output
 rm -r "$3"_input
